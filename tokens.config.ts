b756c8a5621d1155ad156e23660f5c2f90188b18
KristalliDev <kristal.dev@proton.me>
2023-08-13 17:26:16 +0545

    i

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,14 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  alpine: {
+    body: {
+      // Update the background color in light & dark mode
+      backgroundColor: {
+        initial: '#f8fafc',
+        dark: '#0f172a',
+        light: '#f8fafc'
+      }
+    }
+  }
 })
