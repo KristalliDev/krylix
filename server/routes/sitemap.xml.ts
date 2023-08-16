48fb0d4b351e9517d2fb85670b98a41d751ca90f
KristalliDev <kristal.dev@proton.me>
2023-08-16 21:34:06 +0545

    n

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
   // Fetch all documents
   const docs = await serverQueryContent(event).find()
   const sitemap = new SitemapStream({
-    hostname: 'https://krylix.vercel.app'
+    hostname: 'https://krylix.vercel.app/'
   })
 
   for (const doc of docs) {
